9cf69a6c3695964194e3bf982f34c65c1815e79d
joshidhawal <joshidhawal2046@gmail.com>
2019-09-22 10:00:18 +0530

    GreyAtom Challenge
    Simple questions to  be solved for GreyAtrom Beta program that I had solved.

---
+++ ABCheck.py
@@ -0,0 +1,40 @@
+""" 
+Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
+Sample Test Cases
+
+Input:"after badly"
+
+Output:false
+
+Input:"Laura sobs"
+
+Output:true
+"""
+
+str=raw_input()
+str=str.replace(" ","")
+print (str)
+
+a=[]
+b=[]
+counter=0
+
+for i in range(len(str)):
+	if(str[i]=='a'):
+		a.append(i)
+	if(str[i]=='b'):
+		b.append(i)
+
+
+print "a is",a
+print "b is",b
+
+for i in a:
+	for j in b:
+		if(abs(i-j)==3):
+			counter=counter+1
+
+if (counter==0):
+	print("false");
+else:
+	print("true");
---
+++ ThreeFiveMultiples.py
@@ -0,0 +1,27 @@
+"""
+Have the function ThreeFiveMultiples(num) return the sum of all the multiples of 3 and 5 that are below num. For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, and 9, and adding them up you get 23, so your program should return 23. The integer being passed will be between 1 and 100.
+Sample Test Cases
+
+Input:6
+
+Output:8
+
+Input:1
+
+Output:0
+
+
+"""
+
+num=int(raw_input("Enter a number between 1 and 100\n"))
+
+if (num>100 or num<0):
+	print("enter a valid number")
+else:
+
+	sum=0
+	for i in range(num):
+		if (i%3==0 or i%5==0):
+			sum=sum+i
+
+	print(sum)
