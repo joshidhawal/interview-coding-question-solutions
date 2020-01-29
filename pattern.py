d2cc037b2c2d09ab75bb72ff537c56e7a79c1020
joshidhawal <joshidhawal2046@gmail.com>
2020-01-29 08:27:04 +0530

    minor changes to pattern

--- pattern.py
+++ pattern.py
@@ -1,3 +1,13 @@
+"""
+This program will print the Pattern taking the number of rows (n) as input from the user.
+if the n=5
+55555
+54444
+54333
+54322
+54321
+
+"""
 n= int(input("Enter the number of rows : "))
 for i in range(n,0,-1):
     for j in range(n,0,-1):
